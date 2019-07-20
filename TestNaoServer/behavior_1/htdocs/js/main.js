d51a5336bb80c4087cf2aea7a2448084c65730c3
Empiratus <joshwhitney47@gmail.com>
2019-07-20 16:16:30 -0600

    Initial GUI

--- TestNaoServer/behavior_1/htdocs/js/main.js
+++ TestNaoServer/behavior_1/htdocs/js/main.js
@@ -1,8 +1,43 @@
 var application = function(){
     RobotUtils.onService(function(ALTextToSpeech) {
+        this.ALTextToSpeech = ALTextToSpeech;
+
         // Bind button callbacks
-        $(".naoSpeak").click(function() {
-            ALTextToSpeech.say($(this).html());
+        $("#sendInputButton").click(function() {
+            handleInput();
         });
+
+        $("#inputTextArea").keypress((event) => {
+            const keycode = (event.keyCode ? event.keyCode : event.which);
+            if(keycode == '13') {
+                event.preventDefault();
+                handleInput();
+            }
+        });
+
     });
 }
+
+function handleInput() {
+    const inputText = $("#inputTextArea").val();
+    // parseInput();
+    console.log(inputText);
+    say(inputText);
+    addToHistory(inputText);
+    clearTextInput();
+}
+
+function say(utterance) {
+    ALTextToSpeech.say(utterance);
+}
+
+function addToHistory(inputText) {
+    $("#historyTextArea").append(inputText + "&#13;&#10;"); 
+}
+
+function clearTextInput() {
+    $("#inputTextArea").focus().val('').selectRange(0,0);
+}
+
+
+
