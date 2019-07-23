3449fe6adbcf57719b2852b6522ee4953146160b
QuinnyB <boser@ualberta.ca>
2019-07-22 23:23:13 -0600

    Just added some comments to main.js

--- TMI/html/js/main.js
+++ TMI/html/js/main.js
@@ -1,27 +1,28 @@
 var application = function(){
     RobotUtils.onService(function(ALTextToSpeech) {
+        // Adding ALTextTpSpeech to global scope - should probably handle this better eventually
         this.ALTextToSpeech = ALTextToSpeech;
 
-        // Bind button callbacks
+        // Bind "Submit" input button callback
         $("#sendInputButton").click(function() {
             handleInput();
         });
 
+        // When focussed on input text field, bind Enter key press to same callback as Submit button
         $("#inputTextArea").keypress((event) => {
             const keycode = (event.keyCode ? event.keyCode : event.which);
             if(keycode == '13') {
-                event.preventDefault();
+                event.preventDefault();     // Prevent line return behaviour
                 handleInput();
             }
         });
-
     });
 }
 
 function handleInput() {
     const inputText = $("#inputTextArea").val();
+    // console.log(inputText);
     // parseInput();
-    console.log(inputText);
     say(inputText);
     addToHistory(inputText);
     clearTextInput();
@@ -29,14 +30,16 @@ function handleInput() {
 
 function say(utterance) {
     ALTextToSpeech.say(utterance);
+    console.log(utterance);
 }
 
 function addToHistory(inputText) {
+    // Add input text to history and go to next line
     $("#historyTextArea").append(inputText + "&#13;&#10;");
 }
 
 function clearTextInput() {
-    $("#inputTextArea").focus().val('').selectRange(0,0);
+    $("#inputTextArea").val('').focus();
 }
 
 
