a79c2e0af7570ca3c64f03dd19bb0595b3e2ff35
QuinnyB <boser@ualberta.ca>
2019-07-26 00:09:49 -0600

    First pass at async submitting/handlign input

--- TMI/html/js/main.js
+++ TMI/html/js/main.js
@@ -1,36 +1,62 @@
-var application = function(){
-    RobotUtils.onService(function(ALTextToSpeech) {
-        // Adding ALTextTpSpeech to global scope - should probably handle this better eventually
+var application = function () {
+    this.robotReady = true; // State of robot, false if currently executing input
+
+    RobotUtils.onService(function (ALTextToSpeech) {
+    // Adding ALTextTpSpeech to global scope - should probably handle this better eventually
         this.ALTextToSpeech = ALTextToSpeech;
 
         // Bind "Submit" input button callback
-        $("#sendInputButton").click(function() {
-            handleInput();
+        $("#sendInputButton").click(function () {
+            submitInput();
         });
 
-        // When focussed on input text field, bind Enter key press to same callback as Submit button
+        // Bind Enter key press to same callback as Submit button, when focussed on input text field, 
         $("#inputTextArea").keypress((event) => {
             const keycode = (event.keyCode ? event.keyCode : event.which);
-            if(keycode == '13') {
+            if (keycode == '13') {
                 event.preventDefault();     // Prevent line return behaviour
-                handleInput();
+                submitInput();
             }
         });
+
     });
 }
 
-function handleInput() {
+function submitInput() {
     const inputText = $("#inputTextArea").val();
-    // console.log(inputText);
-    // parseInput();
-    say(inputText);
     addToHistory(inputText);
     clearTextInput();
+    if (this.robotReady) {
+        inputManager();
+    }
 }
 
-function say(utterance) {
-    ALTextToSpeech.say(utterance);
-    console.log(utterance);
+async function inputManager() {
+    this.robotReady = false;
+    // Check if there is an active row in history table
+    const lastActiveRow = $('.table-info');
+    let activeRow;
+    if (lastActiveRow.length) {
+        // Yes: Remove from current row and add to next row and get input text from that row 
+        lastActiveRow.removeClass('table-info');
+        activeRow = lastActiveRow.next();
+    }
+    else {
+        // No: Add it to first row and get inputText from first row
+        activeRow = $("#historyList tbody tr").first()
+    }
+    activeRow.addClass('table-info');
+    await handleInput($("#historyList tbody .table-info td").text());
+    // Check if there is a row below the current row in input history
+    if (activeRow.next().length) {
+        // Yes: call self
+        inputManager();
+    }
+    else {
+        // No: this.robotReady = true
+        this.robotReady = true;
+        console.log("Robot ready");
+    }
 }
 
 function addToHistory(inputText) {
@@ -42,12 +68,27 @@ function addToHistory(inputText) {
             <th scope="row">` + (count + 1) + `</th>
             <td>` + inputText + `</td>
         </tr>`
-     );
-
+    );
 }
 
 function clearTextInput() {
     $("#inputTextArea").val('').focus();
 }
 
+async function handleInput(inputText) {
+    // const inputText = $("#inputTextArea").val();
+    // console.log(inputText);
+    // parseInput();
+    console.log('Taking a break...');
+    await sleep(2000);
+    say(inputText);
+}
 
+function say(utterance) {
+    ALTextToSpeech.say(utterance);
+    console.log(utterance);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
