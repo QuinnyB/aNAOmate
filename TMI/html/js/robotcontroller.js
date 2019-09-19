370896b092dad9e099b6f9664cce40c6eb963698
Empiratus <joshwhitney47@gmail.com>
2019-09-18 20:44:46 -0600

    Resolved merge conflicts

--- TMI/html/js/robotcontroller.js
+++ TMI/html/js/robotcontroller.js
@@ -1,14 +1,9 @@
 function RobotController() {
-<<<<<<< HEAD
-
     this.init = function (onFinishLoading) {
-=======
-    this.init = function(onFinishLoading) {
->>>>>>> 15c42eb92027139299cab64e04e76092d793aa63
         this.robotReady = true;
         // this.behaviors = ["Stand/Emotions/Negative/Angry_1", "Stand/Emotions/Negative/Angry_2", "Stand/Emotions/Negative/Angry_3", "Stand/Emotions/Negative/Angry_4", "Stand/Emotions/Negative/Anxious_1", "Stand/Emotions/Negative/Bored_1", "Stand/Emotions/Negative/Bored_2", "Stand/Emotions/Negative/Disappointed_1", "Stand/Emotions/Negative/Exhausted_1", "Stand/Emotions/Negative/Exhausted_2", "Stand/Emotions/Negative/Fear_1", "Stand/Emotions/Negative/Fear_2", "Stand/Emotions/Negative/Fearful_1", "Stand/Emotions/Negative/Frustrated_1", "Stand/Emotions/Negative/Humiliated_1", "Stand/Emotions/Negative/Hurt_1", "Stand/Emotions/Negative/Hurt_2", "Stand/Emotions/Negative/Late_1", "Stand/Emotions/Negative/Sad_1", "Stand/Emotions/Negative/Sad_2", "Stand/Emotions/Negative/Shocked_1", "Stand/Emotions/Negative/Sorry_1", "Stand/Emotions/Negative/Surprise_1", "Stand/Emotions/Negative/Surprise_2", "Stand/Emotions/Negative/Surprise_3", "Stand/Emotions/Neutral/Alienated_1", "Stand/Emotions/Neutral/Annoyed_1", "Stand/Emotions/Neutral/AskForAttention_1", "Stand/Emotions/Neutral/AskForAttention_2", "Stand/Emotions/Neutral/AskForAttention_3", "Stand/Emotions/Neutral/Cautious_1", "Stand/Emotions/Neutral/Confused_1", "Stand/Emotions/Neutral/Determined_1", "Stand/Emotions/Neutral/Embarrassed_1", "Stand/Emotions/Neutral/Hello_1", "Stand/Emotions/Neutral/Hesitation_1", "Stand/Emotions/Neutral/Innocent_1", "Stand/Emotions/Neutral/Lonely_1", "Stand/Emotions/Neutral/Mischievous_1", "Stand/Emotions/Neutral/Puzzled_1", "Stand/Emotions/Neutral/Sneeze", "Stand/Emotions/Neutral/Stubborn_1", "Stand/Emotions/Neutral/Suspicious_1", "Stand/Emotions/Positive/Amused_1", "Stand/Emotions/Positive/Confident_1", "Stand/Emotions/Positive/Ecstatic_1", "Stand/Emotions/Positive/Enthusiastic_1", "Stand/Emotions/Positive/Excited_1", "Stand/Emotions/Positive/Excited_2", "Stand/Emotions/Positive/Excited_3", "Stand/Emotions/Positive/Happy_1", "Stand/Emotions/Positive/Happy_2", "Stand/Emotions/Positive/Happy_3", "Stand/Emotions/Positive/Hungry_1", "Stand/Emotions/Positive/Hysterical_1", "Stand/Emotions/Positive/Interested_1", "Stand/Emotions/Positive/Interested_2", "Stand/Emotions/Positive/Laugh_1", "Stand/Emotions/Positive/Laugh_2", "Stand/Emotions/Positive/Laugh_3", "Stand/Emotions/Positive/Mocker_1", "Stand/Emotions/Positive/Optimistic_1", "Stand/Emotions/Positive/Peaceful_1", "Stand/Emotions/Positive/Proud_1", "Stand/Emotions/Positive/Proud_2", "Stand/Emotions/Positive/Proud_3", "Stand/Emotions/Positive/Relieved_1", "Stand/Emotions/Positive/Shy_1", "Stand/Emotions/Positive/Shy_2", "Stand/Emotions/Positive/Sure_1"];
 
-        RobotUtils.onServices(async function(
+        RobotUtils.onServices(async function (
             ALMemory,
             ALTextToSpeech,
             ALBehaviorManager,
@@ -40,25 +35,16 @@ function RobotController() {
     };
 
     // Execute command on robot
-    this.executeCommand = function(command) {
+    this.executeCommand = function (command) {
         switch (command) {
             case 'wake':
                 ALMotion.wakeUp();
-<<<<<<< HEAD
-                ALRobotPosture.goToPosture("Stand", 1.0);
-                ALAutonomousLife.setState("interactive");
-                break;
-            case "sit":
-                ALRobotPosture.goToPosture("Sit", 1.0);
-            case "rest":
-=======
                 ALRobotPosture.goToPosture('Stand', 1.0);
                 // ALAutonomousLife.setState("disabled");
                 break;
             case 'sit':
                 ALRobotPosture.goToPosture('Sit', 1.0);
             case 'rest':
->>>>>>> 15c42eb92027139299cab64e04e76092d793aa63
                 ALMotion.rest();
                 ALAutonomousLife.setState('solitary');
                 break;
@@ -82,7 +68,7 @@ function RobotController() {
     };
 
     // Split animation into behavior and/or speech, then do/say both
-    this.animate = async function(animation) {
+    this.animate = async function (animation) {
         anitoks = animation.split(';');
         if (anitoks[1] != undefined) {
             behavior = anitoks[0];
@@ -120,15 +106,15 @@ function RobotController() {
         }
     };
 
-    this.speechStatusChanged = function(value) {
+    this.speechStatusChanged = function (value) {
         console.log(value);
     };
 
     // Query status of robot
-    this.getStatus = async function() {
+    this.getStatus = async function () {
         var textDone, animationDone;
 
-        await ALMemory.getData('ALTextToSpeech/TextDone').then(function(TextDone) {
+        await ALMemory.getData('ALTextToSpeech/TextDone').then(function (TextDone) {
             textDone = TextDone;
         });
 
@@ -149,7 +135,7 @@ function RobotController() {
     };
 
     // Send speech to robot to say
-    this.say = function(utterance) {
+    this.say = function (utterance) {
         // ALBehaviorManager.runBehavior("Stand/Emotions/Neutral/Hello_1");
         ALTextToSpeech.say(utterance);
         console.log(utterance);
